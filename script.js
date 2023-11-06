6f79fe96c7b1654d64aff7a7dadd549f30343a18
citronoiseau <avroraka2004@gmail.com>
2023-11-06 16:51:52 +0200

    added mouse logic

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const gridContainer = document.querySelector(`.grid-container`);
 let size = 16;
+let isDrawing = false;
+const resetBtn = document.querySelector(`.reset-button`);
 
 function createGrid() {
   for (let i = 0; i < size * size; i++) {
     const divElement = document.createElement(`div`);
     divElement.classList.add(`element`);
     divElement.style.flexBasis = `${100 / size}%`;
-    divElement.style.height = `${100 / size}%`;
     gridContainer.appendChild(divElement);
 
-    divElement.addEventListener(`mouseover`, changeColor);
+    divElement.addEventListener("mouseover", function (event) {
+      if (event.buttons === 1) {
+        changeColor(event);
+      }
+    });
   }
+
+  function resetGrid() {
+    gridContainer.textContent = "";
+    createGrid();
+  }
+
+  resetBtn.addEventListener(`click`, resetGrid);
 }
 function changeColor(event) {
   event.target.style.backgroundColor = "#131313";
